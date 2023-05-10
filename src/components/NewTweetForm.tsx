7a005b18ae1ab398af955950080615d27f55a840
Aymen <70597520+gitaymen@users.noreply.github.com>
2023-05-10 22:23:01 +0100

    fix warning

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,11 +1,6 @@
 import { useSession } from "next-auth/react";
-import {
-  FormEvent,
-  useCallback,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
+import type { FormEvent } from "react";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
 import { api } from "~/utils/api";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
